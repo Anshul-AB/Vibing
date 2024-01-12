ed433a023676221be8849309ed9a3c7e727d6df6
anshul-999 <anshul.b100@gmail.com>
2024-01-12 15:43:43 +0530

    undo changes

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-require('./connection.js/connection');
-const passport = require('./auth-jwt/auth-jwt');
+const cors = require("cors");
+require("dotenv").config();
+require("./connection.js/connection");
+const passport = require("./auth-jwt/auth-jwt");
 //ROUTES PATH
-const authRoutes = require('./routes/auth');
-const songRoutes = require('./routes/song');
-const playlistRoutes = require('./routes/playlist');
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/song");
+const playlistRoutes = require("./routes/playlist");
 
-const path = require('path');
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-// Serve the index.html for all other routes to enable client-side routing
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname,'frontend', 'build', 'index.html'));
-});
+// const path = require('path');
+// // Serve static files from the 'build' directory
+// app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+// // Serve the index.html for all other routes to enable client-side routing
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname,'frontend', 'build', 'index.html'));
+// });
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res)=>{
-    res.send('hello world');
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 //Routes Middleware
-app.use('/auth', authRoutes);
-app.use('/song', songRoutes);
-app.use('/playlist', playlistRoutes);
+app.use("/auth", authRoutes);
+app.use("/song", songRoutes);
+app.use("/playlist", playlistRoutes);
 
 // Use passport middleware
 app.use(passport.initialize());
 
-app.listen(port, ()=>{
-    console.log(`Server is listening at port`, + port);
-})
+app.listen(port, () => {
+  console.log(`Server is listening at port`, +port);
+});
